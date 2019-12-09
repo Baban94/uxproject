49c5aac8e53792f197b9f183254dc2ab703a9ddb
Baban94 <Zhir.baban.gbg@gmail.com>
2019-12-09 13:52:40 +0100

    Updates

---
+++ precache-manifest.c950ce439a68de6a6734adb8c3a18b03.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0094497f9672e1918631fc8ca0c4d3f",
+    "url": "/uxproject/index.html"
+  },
+  {
+    "revision": "0b59e533cced4f70046e",
+    "url": "/uxproject/static/css/2.d38f0af1.chunk.css"
+  },
+  {
+    "revision": "434ff599ae8068bf79a5",
+    "url": "/uxproject/static/css/main.53429628.chunk.css"
+  },
+  {
+    "revision": "0b59e533cced4f70046e",
+    "url": "/uxproject/static/js/2.d4f9e53f.chunk.js"
+  },
+  {
+    "revision": "434ff599ae8068bf79a5",
+    "url": "/uxproject/static/js/main.90d382d5.chunk.js"
+  },
+  {
+    "revision": "1f59631bb1efcbed9682",
+    "url": "/uxproject/static/js/runtime-main.2a809690.js"
+  },
+  {
+    "revision": "6cb72d402e9b2993cdf6433e455688a6",
+    "url": "/uxproject/static/media/Earthlybild.6cb72d40.png"
+  },
+  {
+    "revision": "03dd63568a9b782931fae375315d1aac",
+    "url": "/uxproject/static/media/Polarbear.03dd6356.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/uxproject/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/uxproject/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/uxproject/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/uxproject/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/uxproject/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/uxproject/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/uxproject/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/uxproject/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/uxproject/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/uxproject/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/uxproject/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/uxproject/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/uxproject/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/uxproject/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/uxproject/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/uxproject/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
